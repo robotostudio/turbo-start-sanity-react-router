b6d29d706f2aeeae402a7e7675de265b96442db3
SameerJSRS <sameer@robotostudio.com>
2025-12-02 13:22:14 +0530

    refactor: improve formatting and structure of validation error handling in environment configuration

--- apps/web/app/env/server.ts
+++ apps/web/app/env/server.ts
@@ -9,13 +9,14 @@ export const serverEnv = createEnv({
       .default("development"),
     SANITY_API_READ_TOKEN: z.string().min(1),
     SANITY_API_WRITE_TOKEN: z.string().min(1),
-    SANITY_SESSION_SECRET: z.string().min(1).default("e5670a7f1b59e932")
+    SANITY_SESSION_SECRET: z.string().min(1).default("e5670a7f1b59e932"),
   },
 
   emptyStringAsUndefined: true,
   runtimeEnv: typeof process !== "undefined" ? process.env : {},
   onValidationError(issues) {
     const errorMessage = formatValidationError(issues);
+    // biome-ignore lint/suspicious/noConsole: error message
     console.error(errorMessage);
     throw new Error(errorMessage);
   },
